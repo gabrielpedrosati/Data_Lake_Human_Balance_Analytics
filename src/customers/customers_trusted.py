43ecd4b7b8b90f7ce02473766ba877cba3813a70
Gabriel Pedrosa <gabrielpedrosati@gmail.com>
2023-05-22 22:21:58 -0300

    fix: customers landing and customers trusted images and modified customers_trusted.py

--- src/customers/customers_trusted.py
+++ src/customers/customers_trusted.py
@@ -4,26 +4,46 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
-from pyspark.sql import SparkSession
+import re
 
-## @params: [JOB_NAME]
-args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
+args = getResolvedOptions(sys.argv, ["JOB_NAME"])
 sc = SparkContext()
 glueContext = GlueContext(sc)
+spark = glueContext.spark_session
 job = Job(glueContext)
-job.init(args['JOB_NAME'], args)
+job.init(args["JOB_NAME"], args)
+
+# Script generated for node Landing Customer Zone
+LandingCustomerZone_node1 = glueContext.create_dynamic_frame.from_options(
+    format_options={},
+    connection_type="s3",
+    format="json",
+    connection_options={
+        "paths": ["s3://data-lake-udacity-524095156763/customers/landing/"],
+        "recurse": True,
+    },
+    transformation_ctx="LandingCustomerZone_node1",
+)
+
+# Script generated for node PrivacyFilter
+PrivacyFilter_node1684804342105 = Filter.apply(
+    frame=LandingCustomerZone_node1,
+    f=lambda row: (not (row["shareWithResearchAsOfDate"] == 0)),
+    transformation_ctx="PrivacyFilter_node1684804342105",
+)
+
+# Script generated for node Trusted Customer Zone
+TrustedCustomerZone_node3 = glueContext.getSink(
+    path="s3://data-lake-udacity-524095156763/customers/trusted/",
+    connection_type="s3",
+    updateBehavior="UPDATE_IN_DATABASE",
+    partitionKeys=[],
+    enableUpdateCatalog=True,
+    transformation_ctx="TrustedCustomerZone_node3",
+)
+TrustedCustomerZone_node3.setCatalogInfo(
+    catalogDatabase="stedi", catalogTableName="customers_trusted"
+)
+TrustedCustomerZone_node3.setFormat("json")
+TrustedCustomerZone_node3.writeFrame(PrivacyFilter_node1684804342105)
 job.commit()
-
-from pyspark.sql import SparkSession
-
-spark = SparkSession \
-        .builder \
-        .appName("Customers Trusted Zone") \
-        .getOrCreate()
-
-customers = spark.read.json("s3://data-lake-udacity-524095156763/customers/landing/")
-
-customers_agreed_research = customers.where(customers.shareWithResearchAsOfDate.isNotNull())
-
-customers_agreed_research.write.parquet("s3://data-lake-udacity-524095156763/customers/trusted/")
